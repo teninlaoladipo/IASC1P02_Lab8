c68630af2e0c6fe42f7de265d0c19a3a89e46bea
Teninla <to16ij@brocku.ca>
2019-11-08 11:36:05 -0500

    Initial commit

---
+++ lab8-2.js
@@ -0,0 +1,17 @@
+//Sets the initial colours of the three elements on page load
+
+//A function to change colours of elements when called
+function mix(){
+	document.getElementById("blue").style.color="blue";
+	document.getElementById("green").style.color="green";
+	document.getElementById("red").style.color="red";
+
+	var first= "blue";
+	var second= "green";
+	var third= "red";
+
+	document.getElementById("blue").style.color=first;
+	document.getElementById("green").style.colour=second;
+	document.getElementById("red").style.color=third;
+
+}
